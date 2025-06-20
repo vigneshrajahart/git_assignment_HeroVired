11f41683263df213170dd8060e575497f507f283
vignesh Raja <vignesh.vv11@gmail.com>
2025-06-21 00:54:22 +0530

    updated Calc file

--- calculator.py
+++ calculator.py
@@ -2,16 +2,20 @@ import math
 class Calculator:
     def add(self, a, b):
         return a + b
+    
     def subtract(self, a, b):
         return a - b
+    
     def multiply(self, a, b):
         return a * b
+    
     def divide(self, a, b):
      if b == 0:
         raise ValueError("Cannot divide by zero.")
      return a / b
     def square_root(self, x):
         return math.sqrt(x)
+    
 if __name__ == "__main__":
     calculator = Calculator()
     num1 = 16
